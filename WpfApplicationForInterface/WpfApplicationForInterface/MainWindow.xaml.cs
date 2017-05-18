5d6acb73e5deb125efa8275e310a1214114aa5c3
Normall777 <normall777@yandex.ru>
2017-05-18 18:30:32 +0500

    Начальный функционал сохранения

--- WpfApplicationForInterface/WpfApplicationForInterface/MainWindow.xaml.cs
+++ WpfApplicationForInterface/WpfApplicationForInterface/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +15,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Serialization;
+using WpfApplicationForInterface.Data;
 
 namespace WpfApplicationForInterface
 {
@@ -23,9 +28,66 @@ namespace WpfApplicationForInterface
         public MainWindow()
         {
             InitializeComponent();
+            //Binding binding = BindingOperations.GetBinding(myTextBlock, TextBlock.TextProperty);
+            Binding binding = new Binding();
+            binding.ElementName = "myTextBox";
+            binding.Path = new PropertyPath("Text");
+            myTextBlock.SetBinding(TextBlock.TagProperty, binding);
         }
 
-        private void ListBox_AccessKeyPressed(object sender, AccessKeyPressedEventArgs e)
+        private void Button_Shifrovka(object sender, RoutedEventArgs e)
+        {
+            string text = myTextBlock.Text;
+            int TypeOfEnicrypt = enicrypt.SelectedIndex;
+        }
+
+        public EnicryptionText GetObject()
+        {
+            MD5 md5Hasher = MD5.Create();
+            byte[] data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(myTextBlock.Text));
+            StringBuilder sBuilder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+            return new EnicryptionText
+            {
+                Text = myTextBlock.Text,
+                Hash = sBuilder.ToString()
+            };
+        }
+
+        private void Button_Rashifrovka(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Create(object sender, RoutedEventArgs e)
+        {
+            myTextBlock.Text = "";
+            keyBox.Text = "";
+        }
+
+        private void MenuItem_Open(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Save(object sender, RoutedEventArgs e)
+        {
+            var sfd = new SaveFileDialog() { Filter = "*.secret|*.secret" };
+            if ((bool)sfd.ShowDialog(this))
+            {
+                var data = GetObject();
+                var xs = new XmlSerializer(typeof(EnicryptionText));
+                using (var fileStream = File.Create(sfd.FileName))
+                {
+                    xs.Serialize(fileStream, data);
+                }
+            }
+        }
+
+        private void MenuItem_Exit(object sender, RoutedEventArgs e)
         {
 
         }
